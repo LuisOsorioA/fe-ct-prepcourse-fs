2ec627c0345189084c37129219c24f3862f6971c
LuisOsorioA <124949841+LuisOsorioA@users.noreply.github.com>
2023-02-17 20:32:25 -0500

    17022023-8_31pm
    17022023-8_31pm

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,43 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   var cuadrado = Math.pow(num, 2);
+   return cuadrado;
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+  var cubo = Math.pow(num, 3);
+  return cubo;
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+  var elevarA = Math.pow(num, exponent);
+  return elevarA;
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var round = Math.round(num);
+   return round;
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   var roundUp = Math.ceil(num);
+   return roundUp;
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var random = Math.random();
+   return random;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
