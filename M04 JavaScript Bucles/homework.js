f831d8b4fb90d022b84da0a9a4d5654f05a4a99e
LuisOsorioA <LUISOSORIO@example.com>
2023-02-21 15:35:51 -0500

    pruebaLanix

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,14 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   if (x > y){
+      return x
+   }else if (y > x) {
+      return y
+   }else if(x === y){
+      return x || y      
+   } 
+
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +20,11 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if (edad >= 18){
+      return "Allowed"
+   }else{ 
+      return "Not allowed"
+   }
 }
 
 function conection(status) {
@@ -21,6 +34,11 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   if (status === 1){
+   return "Online"}
+   if (status === 2){
+      return "Away"}
+      else {return "Offline"}
 }
 
 function saludo(idioma) {
@@ -30,6 +48,16 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   if (idioma == "aleman"){
+      return "Guten Tag!"}
+   if (idioma == "mandarin"){
+      return "Ni Hao!"}
+   if (idioma == "ingles"){
+      return "Hello!"}
+      else{
+       return "Hola!"  
+      }
+
 }
 
 function colors(color) {
@@ -41,6 +69,7 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+      
 }
 
 function esDiezOCinco(num) {
